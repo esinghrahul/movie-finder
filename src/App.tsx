78b2c984e4ea5c1a7a6737fce21308bdd4fc4339
esinghrahul <e.singh.rahul@outlook.com>
2020-10-20 17:55:47 +0530

    Added Header component using material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Button} from '@material-ui/core';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">Hello World</Button>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {AppBar, Toolbar, IconButton, Typography, InputBase, StylesProvider, makeStyles, Theme, createStyles, fade} from '@material-ui/core';
+import {Menu as MenuIcon, Search as SearchIcon} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        root: {
+            flexGrow: 1,
+          },
+          menuButton: {
+            marginRight: theme.spacing(2),
+          },
+          title: {
+            flexGrow: 1,
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+              display: 'block',
+            },
+          },
+          search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+              backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+              marginLeft: theme.spacing(1),
+              width: 'auto',
+            },
+          },
+          searchIcon: {
+            padding: theme.spacing(0, 2),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+          inputRoot: {
+            color: 'inherit',
+          },
+          inputInput: {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+              width: '12ch',
+              '&:focus': {
+                width: '20ch',
+              },
+            },
+          }
+    }))
+
+export const Header : React.FC = props => {
+    const styles = useStyles();
+    return(
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={styles.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={styles.title} variant="h6" noWrap>
+            Movie Finder
+          </Typography>
+          <div className = {styles.search}>
+            <div className = {styles.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes = {{
+                root: styles.inputRoot,
+                input: styles.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    )
+}
